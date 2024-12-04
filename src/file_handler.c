f4184736933d6b9601877ec9efe8a878a4cc6cf9
yaellc <yael.laforcecardot@gmail.com>
2024-12-04 09:31:02 +0000

    modifs

--- src/file_handler.c
+++ src/file_handler.c
@@ -12,6 +12,42 @@ log_t read_backup_log(const char *logfile){
     * @param: logfile - le chemin vers le fichier .backup_log
     * @return: une structure log_t
     */
+    log_t logs = {NULL, NULL};
+    FILE *file = fopen(logfile, "r");
+    if (!file) {
+        perror("Failed to open backup_log");
+        return logs;
+    }
+    char line[256];
+    while (fgets(line, sizeof(line), file)) {
+        log_element *new_element = malloc(sizeof(log_element));
+        if (!new_element) {
+            perror("Failed to allocate memory");
+            fclose(file);
+            return logs;
+        }
+        char md5_str[MD5_DIGEST_LENGTH * 2 + 1];
+        new_element->date = malloc(20);
+
+        sscanf(line, "%ms %s %19s", &new_element->path, md5_str, new_element->date);
+
+        for (int i = 0; i < MD5_DIGEST_LENGTH; ++i) {
+            sscanf(md5_str + i * 2, "%2hhx", &new_element->md5[i]);
+        }
+
+        new_element->next = NULL;
+        new_element->prev = logs.tail;
+
+        if (logs.tail) {
+            logs.tail->next = new_element;
+        } else {
+            logs.head = new_element;
+        }
+        logs.tail = new_element;
+    }
+
+    fclose(file);
+    return logs;
 }
 
 // Fonction permettant de mettre à jour une ligne du fichier .backup_log
