059eb29e8b3bc0a377ec8bfbde4dcf984c644856
Zhisque <come.bergeretport@gmail.com>
2024-12-08 21:28:34 +0000

    Update from flat

--- src/backup_manager.c
+++ src/backup_manager.c
@@ -21,7 +21,7 @@ void create_backup(const char *source_dir, const char *backup_dir) {
     strftime(str_date, 20, "%Y-%m-%d-%H:%M:%S", localtime(&now));
     
     if (strlen(backup_dir) > 1011) {
-        perror("Directory name is too long!");
+        perror("Directory name is too long!\n");
         return;
     }
 
@@ -34,7 +34,7 @@ void create_backup(const char *source_dir, const char *backup_dir) {
 
     DIR *source = opendir(source_dir);
     if (source == NULL) {
-        perror("Source directory does not exist");
+        perror("Source directory does not exist\n");
         return;
     }
 
@@ -42,9 +42,15 @@ void create_backup(const char *source_dir, const char *backup_dir) {
     while (file != NULL) {
         log_element *element = existing_logs.tail;
         while (element != NULL && strcmp(element->path, file->d_name)) {
-            
             element = element->prev;
         }
+        if (element == NULL) {
+            //Fichier non existant dans la cible
+        } /*else if (element->date) {
+
+        }   Pas fini :(    */
+
+
         file = readdir(source);
     }
 
@@ -53,17 +59,42 @@ void create_backup(const char *source_dir, const char *backup_dir) {
 
 // Fonction permettant d'enregistrer dans fichier le tableau de chunk dédupliqué
 void write_backup_file(const char *output_filename, Chunk *chunks, int chunk_count) {
-    /*
-    */
+    FILE *output_file = fopen(output_filename, "w");
+    if (output_file == NULL) {
+        fprintf(stderr, "Error opening file %s\n", output_filename);
+        return;
+    }
+
+    for (int i = 0; i < chunk_count; ++i) {
+        if(fwrite(chunks[i].md5, sizeof(char), MD5_DIGEST_LENGTH, output_file) != 1) {
+            perror("Failed to write MD5 sum");
+            return;
+        }
+        if (fwrite(chunks[i].data, sizeof(void), CHUNK_SIZE, output_file) != 1) {
+            perror("Failed to write data chunk");
+            return;
+        }
+    }
+    fclose(output_file);
 }
 
 
 // Fonction implémentant la logique pour la sauvegarde d'un fichier
 void backup_file(const char *filename) {
-    /*
-    */
-}
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        perror("Error opening file starting backup");
+        return;
+    }
+    Md5Entry *hash_table = NULL;
+    Chunk *chunks = NULL;
+    deduplicate_file(file, chunks, hash_table);
 
+    char output_filename[1024];//D'où vient le nom de l'output ???
+    write_backup_file(output_filename, chunks, 0);//Comment avoir le nombre ? Quand s'arrête la hash_table ?
+
+    fclose(file);
+}
 
 // Fonction permettant la restauration du fichier backup via le tableau de chunk
 void write_restored_file(const char *output_filename, Chunk *chunks, int chunk_count) {
