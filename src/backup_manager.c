d229b00ec2ea6debed3143c598c17e4b7657c3c7
Zhisque <come.bergeretport@gmail.com>
2024-12-10 14:59:42 +0000

    Update from flat

--- src/backup_manager.c
+++ src/backup_manager.c
@@ -40,12 +40,19 @@ void create_backup(const char *source_dir, const char *backup_dir) {
 
     struct dirent *file = readdir(source);
     while (file != NULL) {
-        log_element *element = existing_logs.tail;
+        log_element *element = existing_logs.head;
         while (element != NULL && strcmp(element->path, file->d_name)) {
-            element = element->prev;
+            element = element->next;
         }
+        unsigned char md5_file[MD5_DIGEST_LENGTH];
+        //JSP
         if (element == NULL) {
             //Fichier non existant dans la cible
+            log_element *new_element = malloc(sizeof(log_element));
+            new_element->path = malloc(sizeof(file->d_name+1));
+            strcpy(new_element->path, file->d_name);
+            strcpy(new_element->md5, md5_file);
+
         } /*else if (element->date) {
 
         }   Pas fini :(    */
@@ -81,25 +88,64 @@ void write_backup_file(const char *output_filename, Chunk *chunks, int chunk_cou
 
 // Fonction implémentant la logique pour la sauvegarde d'un fichier
 void backup_file(const char *filename) {
-    FILE *file = fopen(filename, "r");
-    if (!file) {
-        perror("Error opening file starting backup");
+    if (filename == NULL) {
+        perror("Invalid arguments");
         return;
     }
-    Md5Entry *hash_table = NULL;
-    Chunk *chunks = NULL;
-    deduplicate_file(file, chunks, hash_table);
+    struct stat file_stat;
+    if (stat(filename, &file_stat) != 0) {
+        perror("Stat failed, probable file does not exist");
+        return;
+    }
 
-    char output_filename[1024];//D'où vient le nom de l'output ???
-    write_backup_file(output_filename, chunks, 0);//Comment avoir le nombre ? Quand s'arrête la hash_table ?
+    if (S_ISDIR(file_stat.st_mode)) {
+        DIR *file = opendir(filename);
+        struct dirent *subfile = readdir(file);
+        while (subfile != NULL) {
+            backup_file(subfile);
+            readdir(subfile);
+        }
+        closedir(file);
+    } else if (S_ISREG(file_stat.st_mode)) {
+        FILE *file = fopen(filename, "r");
+        if (!file) {
+            perror("Error opening file starting backup");
+            return;
+        }
+        Md5Entry *hash_table = NULL;
+        Chunk *chunks = NULL;
+        deduplicate_file(file, chunks, hash_table);//Pas sûre de ça
 
-    fclose(file);
+        char output_filename[1024];//D'où vient le nom de l'output ???
+        write_backup_file(output_filename, chunks, sizeof(chunks));
+
+        fclose(file);
+    } else {
+        perror("Unknown file type");
+    }
 }
 
 // Fonction permettant la restauration du fichier backup via le tableau de chunk
 void write_restored_file(const char *output_filename, Chunk *chunks, int chunk_count) {
-    /*
-    */
+    if (output_filename == NULL || chunks == NULL || chunk_count == 0) {
+        perror("Invalid arguments");
+        return;
+    }
+
+    FILE *output_file = fopen(output_filename, "w");
+    if (!output_file) {
+        perror("Error opening file restoring backup");
+        return;
+    }
+
+    for (int i = 0; i < chunk_count; ++i) {
+        if (fwrite(&chunks[i], sizeof(void), CHUNK_SIZE, output_file) != 1) {
+            perror("Error writing file during restoration");
+            fclose(output_file);
+            return;
+        }
+    }
+    fclose(output_file);
 }
 
 // Fonction pour restaurer une sauvegarde
@@ -107,4 +153,11 @@ void restore_backup(const char *backup_id, const char *restore_dir) {
     /* @param: backup_id est le chemin vers le répertoire de la sauvegarde que l'on veut restaurer
     *          restore_dir est le répertoire de destination de la restauration
     */
+    //Lecture logfile
+    if (backup_id == NULL || restore_dir == NULL) {
+        perror("Invalid arguments");
+        return;
+    }
+
+    //
 }
