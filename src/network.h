5235fb7c5d02f87a92b051eb34fd4a5ef9245ce8
Zhisque <come.bergeretport@gmail.com>
2025-01-02 14:02:47 +0000

    network.c/.h

--- src/network.h
+++ src/network.h
@@ -1,8 +1,15 @@
 #ifndef NETWORK_H
 #define NETWORK_H
 
-#include <stdlib.h>
 
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <string.h>
+
+void send_data(const char *server_address, int port, const void *data, size_t size);
+size_t receive_data(int port, size_t size);
 
 #endif // NETWORK_H
-
