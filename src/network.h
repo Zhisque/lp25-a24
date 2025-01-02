d53c3f75e55933f1dd6a9245b29cd133a3549f83
KodjoTossou <112134296+KodjoTossou@users.noreply.github.com>
2025-01-02 12:28:31 +0100

    Update network.h

--- src/network.h
+++ src/network.h
@@ -1,8 +1,16 @@
 #ifndef NETWORK_H
 #define NETWORK_H
 
+
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <string.h>
+
 void send_data(const char *server_address, int port, const void *data, size_t size);
-void receive_data(int port);
+size_t receive_data(int port, size_t size);
 
 #endif // NETWORK_H
 
